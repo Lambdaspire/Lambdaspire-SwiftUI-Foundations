9b47395350d83501c81fa001385cc30a9af76dd2
Skylark <skylark@lambdaspire.com>
2024-07-30 16:03:54 +1000

    Oops

--- Sources/LambdaspireSwiftUIFoundations/ExportedMacros.swift
+++ Sources/LambdaspireSwiftUIFoundations/ExportedMacros.swift
@@ -5,7 +5,7 @@ import SwiftUI
 @attached(extension, conformances: ViewWithViewModel)
 public macro ViewWithViewModel() = #externalMacro(module: "LambdaspireSwiftUIFoundationsMacros", type: "ViewWithViewModelMacro")
 
-@attached(member, names: named(initialise), named(init))
+@attached(member, names: named(initialise), named(init), named(empty))
 @attached(extension, conformances: ViewModel, View)
 public macro ViewModel(generateEmpty: Bool = false) = #externalMacro(module: "LambdaspireSwiftUIFoundationsMacros", type: "ViewModelMacro")
 
