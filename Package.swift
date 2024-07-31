151e3ae7211584afe306ee61d629bcc980f663cf
Skylark <skylark@lambdaspire.com>
2024-07-31 15:18:35 +1000

    Package ref update to Abstractions v2.0.0

--- Package.swift
+++ Package.swift
@@ -19,12 +19,9 @@ let package = Package(
             targets: ["LambdaspireSwiftUIFoundations"]),
     ],
     dependencies: [
-//        .package(
-//            url: "https://github.com/Lambdaspire/Lambdaspire-Swift-Abstractions",
-//            from: "1.0.0"),
         .package(
             url: "https://github.com/Lambdaspire/Lambdaspire-Swift-Abstractions",
-            branch: "v2"),
+            from: "2.0.0"),
         .package(
             url: "https://github.com/apple/swift-syntax.git",
             from: "509.0.0")
